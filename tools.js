9f31efd4c235bc5a1e3b7496ce8b81eb35170396
jakerella <jordan@jordankasper.com>
2014-11-17 18:18:45 -0500

    Added jasmine ajax content

--- tools.js
+++ tools.js
@@ -37,4 +37,65 @@ $.mockjax({
 });
 
 
+// Angular $httpBackend
+
+var testApp = angular.module('testApp', ['beerApp', 'ngMockE2E']);
+
+testApp.run(function($httpBackend) {
+
+    $httpBackend.whenGET('/api/search').respond([]);
+
+
+    $httpBackend.whenGET('/api/search').respond(function(method, url) {
+        var queryParams = customURLToJsonMethod(url);
+
+        if (queryParams.query === 'HTTP-ERROR') {
+            
+            return [500, 'Uh oh...', {}];
+
+        }
+    });
+
+    
+    $httpBackend.whenPOST(/api\/rate\/\d+/)
+        .respond(function(method, url, data) {
+            var beerId = customURLParamExtract(url),
+                postData = JSON.parse(data);
+
+            return [200, {
+                id: beerId,
+                rating: postData.rating,
+                avgRating: 3.75
+            }, {}];
+        });
+
+});
+
+
+// Jasmine
+
+describe('Toggling favorites', function() {
+
+    beforeEach(function() {
+      
+        jasmine.Ajax.install();
+
+        // ...
+
+        jasmine.Ajax.stubRequest('api/search?query=fhqwhgads').andReturn({
+            status: 500,
+            responseText: 'Uh oh...'
+        });
+
+        jasmine.Ajax.stubRequest('api/rate/13', 'rating=5').andReturn({
+            responseText: JSON.stringify({ id: 13, rating: 5 })
+        });
+
+    });
+
+    afterEach(function() {
+        jasmine.Ajax.uninstall();
+    });
+
+});
 
