3476fa5f1f8c3e5ba77e55298ba71d4b761cc886
jakerella <jordan@jordankasper.com>
2014-11-16 19:04:26 -0500

    Adding to the beer sample app and slides

--- beer.js
+++ beer.js
@@ -1,6 +1,6 @@
 window.beerApp = (function(beer) {
     
-    beer.doSearch = function(query, callback) {
+    beer.search = function(query, callback) {
         
         $.ajax({
             url: "api/search",
@@ -18,14 +18,54 @@ window.beerApp = (function(beer) {
         
     };
 
+    beer.toggleFavorite = function(beerId, callback) {
+
+        $.ajax({
+            url: "api/favorite/" + beerId,
+            type: "POST",
+            dataType: "json",
+            success: function searchSuccessHandler(data) {
+                beer.toggleBeerFavIcon(beerId, data.isFavorite);
+                callback(data.isFavorite);
+            },
+            error: function searchErrorHandler() {
+                beer.notify("Sorry, but toggling favorites failed!");
+                callback(null);
+            }
+        });
+
+    };
+
+    beer.rateBrew = function(beerId, rating, callback) {
+
+        $.ajax({
+            url: "api/rate/" + beerId,
+            data: { "rating": rating },
+            type: "POST",
+            dataType: "json",
+            success: function searchSuccessHandler(data) {
+                beer.showBeerRating(beerId, data.rating);
+                callback(data.rating);
+            },
+            error: function searchErrorHandler() {
+                beer.notify("Sorry, but beer rating failed!");
+                callback(null);
+            }
+        });
+
+    };
 
 
     beer.handleSearchResults = function(results) {
-        return; // Obviously this would need to be implemented...
+        return;  // TODO: implement me!
     };
 
     beer.notify = function(msg) {
-        return; // ...this one, too.
+        return;  // TODO: implement me!
+    };
+
+    beer.toggleBeerFavIcon = function(beerId, isFavorite) {
+        return;  // TODO: implement me!
     };
 
     return beer;
